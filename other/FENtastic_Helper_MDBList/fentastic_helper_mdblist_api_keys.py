667324bb88c8a144038d276ed1631b91f5d0e851
kodi7rd <KodiRealDebridIsrael@github.com>
2024-05-13 02:48:25 +0300

    Create fentastic_helper_mdblist_api_keys.py

---
+++ other/FENtastic_Helper_MDBList/fentastic_helper_mdblist_api_keys.py
@@ -0,0 +1,12 @@
+MDBLIST_API_KEYS = {
+    "darksubsil1-api_key": "csq5pq5vhf4r1snagff7qukcu",
+    "darksubsil2-api_key": "m1azvroj6xgt6x05urkmd9m9u",
+    "darksubsil3-api_key": "dbm160enk1t0shknn3r2bh003",
+    "darksubsil4-api_key": "n5p3p7mvdb9tmdq49mrp59tdu",
+    "darksubsil5-api_key": "5608d5y1dkhogghe4ly4nkjr9",
+    "darksubsil6-api_key": "blgzz5pfmfb973mgy1lp4n3s8",
+    "darksubsil7-api_key": "9wcsvxj6gjv7io6p9pcqfeqbw",
+    "darksubsil8-api_key": "udrqs9ni2gdf6qvtnytnodrxt",
+    "darksubsil9-api_key": "j49h9i7aohe7g4pdpl4o5rp38",
+    "darksubsil10-api_key": "kq4ur9et7hceu2h0e00l2hw0j"
+}
